93d3af8d46b298cfc47dd6d7d7afaac6fa72d39c
qubard <qubard@gmail.com>
2018-09-09 23:08:36 -0300

    added problem 2

---
+++ 2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    typedef unsigned int uint;
+    uint a = 1;
+    uint b = 2;
+    uint c = a+b;
+    uint sum = b;
+    while(c < 4000000) {
+        if(c % 2 == 0) sum += c;
+        a = b;
+        b = c;
+        c = a+b;
+    }
+    std::cout << "Solution is " << sum << std::endl;
+    return 0;
+}
