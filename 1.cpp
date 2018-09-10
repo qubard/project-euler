ec03cd7e556f0c697a9f5747a10c1ae61aa1f7b6
qubard <qubard@gmail.com>
2018-09-09 23:52:47 -0300

    added problem 4

--- 1.cpp
+++ 1.cpp
@@ -5,7 +5,7 @@ int main() {
     uint solution = 0;
     uint N = 1000; // all multiples below N
     for(uint i = 0; i < N; i += 3) solution += i;
-    for(uint i = 0; i < N; i+= 5) solution += i;
+    for(uint i = 0; i < N; i += 5) solution += i;
     std::cout << "Solution is " << solution << std::endl;
     return 0;
 }
---
+++ 4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+typedef unsigned int uint;
+
+int main() {
+    uint solution = 1;
+    for(uint a = 999; a > 1; a--) {
+        for(uint b = 1; b < a; b++) {
+            uint res = a*b;
+            std::string palindrome = std::to_string(res);
+            std::string original = std::string(palindrome);
+            std::reverse(palindrome.begin(), palindrome.end());
+            if(res > solution && palindrome == original) {
+                solution = res;
+            }
+        }
+    }
+    std::cout << "Solution is " << solution << std::endl; // 993*913 = 906609
+    return 0;
+}
