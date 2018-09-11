41939493c8d2feedc5d34233276cf21d6d6ecc12
qubard <qubard@gmail.com>
2018-09-11 00:11:07 -0300

    added problem 14

---
+++ 14.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+typedef unsigned long long ulong;
+
+inline ulong collatz(ulong n) {
+    ulong i = 1;
+    for(; n != 1; n = n % 2 == 0 ? n / 2 : 3 * n + 1, i++);
+    return i;
+}
+
+int main() {
+    ulong solution = 0;
+    ulong solution_i = 0;
+    for(ulong i = 1; i < 1000000; i++) { 
+        ulong c = collatz(i);
+        if(c > solution) {
+            solution = c;
+            solution_i = i;
+        }
+    }
+    std::cout << "Solution is " << solution_i << std::endl;
+    return 0;
+}
